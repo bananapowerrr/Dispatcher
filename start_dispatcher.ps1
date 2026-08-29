444d26ffce7b8c42143715c12a2389374047940c
bananapwrrr <138520060+bananapowerrr@users.noreply.github.com>
2026-08-29 21:18:51 +0300

    AgentBus sync 2026-08-29 21:18

---
+++ start_dispatcher.ps1
@@ -0,0 +1,3 @@
+﻿$env:OLLAMA_API_BASE = "http://127.0.0.1:11434"
+Set-Location "G:\Мой диск\AgentBus"
+Start-Process -FilePath "C:\Users\user\AppData\Local\Programs\Python\Python312\python.exe" -ArgumentList "-u","G:\Мой диск\AgentBus\dispatcher.py" -WindowStyle Hidden -RedirectStandardOutput "G:\Мой диск\AgentBus\dispatcher.log" -RedirectStandardError "G:\Мой диск\AgentBus\dispatcher.log"
